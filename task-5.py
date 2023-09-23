6a9986bde1be592ca7529da932b881e6e1dd1ba9
Alexey Nikolaev <84949991+linue-code@users.noreply.github.com>
2023-09-23 09:17:18 +0900

    Update task-5.py

--- task-5.py
+++ task-5.py
@@ -1,8 +1,12 @@
-for n in range(1,1000):
+res = []
+for n in range(1, 1000):
   s = bin(n)[:2]
-  if s % 2 == 0:
-    processed_binary = '1 + s + 0'
-  else:
-    processed_binary = '11 + s + 00'
-  return int(processed_binary,2)
   
+  if n % 2 == 0:
+    binary = '1 + s + 0'
+  else:
+    binary = '11 + s + 00'
+  if int(s, 2) < 52:
+    
+    res.append(int(s, 2))
+print(min(res))
