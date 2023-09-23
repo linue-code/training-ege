e903437d219c8542a07dec7f8b75c6ce3792a3df
Alexey Nikolaev <84949991+linue-code@users.noreply.github.com>
2023-09-23 09:05:29 +0900

    Update task-5.py

--- task-5.py
+++ task-5.py
@@ -1,1 +1,8 @@
-
+for n in range(1,1000):
+  s = bin(n)[:2]
+  if s % 2 == 0:
+    processed_binary = '1 + s + 0'
+  else:
+    processed_binary = '11 + s + 00'
+  return int(processed_binary,2)
+  
